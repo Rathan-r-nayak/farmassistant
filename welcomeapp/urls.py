4d2b2141359b1271d25f57bd5fcd9aeb082eaf93
Rathan-r-nayak <rathannayak300@gmail.com>
2023-04-25 13:18:54 +0530

    created database tables and put that in models.py file as python code and included and url to handle discuss link

--- welcomeapp/urls.py
+++ welcomeapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.welcomePage,name='welcomePage'),
     path('signinSubmit',views.loginSubmit,name='loginSubmit'),
-    path('signupSubmit',views.signupSubmit,name='signupSubmit')
+    path('signupSubmit',views.signupSubmit,name='signupSubmit'),
 ]
