7f010b6e265a676069364112ecdd67216dd0e5ed
Rathan-r-nayak <rathannayak300@gmail.com>
2023-04-25 14:33:16 +0530

    added url and method to handle discuss app proper functioning that is the discussion can done

--- discussapp/urls.py
+++ discussapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.discuss,name='discusspage'),
+    path('ask_question',views.askQuestion,name='queryPage'),
+    path('questionSubmit',views.questionSubmit,name='questionSubmit'),
+    path('answerSubmit',views.answerSubmit,name='answerSubmit'),
 ]
--- discussapp/views.py
+++ discussapp/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . import models
+from django.template import loader
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 def discuss(request):
-    pass
+    query=models.Questions.objects.all().values()
+    answer=models.Answers.objects.all().values()
+    context={
+        'queries':query,
+        'ans':answer
+    }
+    template = loader.get_template('discussapp/discussion.html')
+    return HttpResponse(template.render(context, request))
+    # return render(request,'discussapp/discussion.html')
+
+def answerSubmit(request):
+    if request.method=='POST':
+        ans=request.POST['ans']
+        qid=int(request.POST['fid_qid'])
+
+        farmer=models.Farmer.objects.get(fid=2)
+        ob=models.Answers(query_id=qid,fid=farmer,answer=ans)
+        ob.save()
+        return redirect('discusspage')
+    return redirect('discusspage')
+
+
+def askQuestion(request):
+    return render(request,'discussapp/askQuestion.html')
+
+
+def questionSubmit(request):
+    if request.method=='POST':
+        query=request.POST['question']
+
+        
+        farmer=models.Farmer.objects.get(fid=2)
+        ob=models.Questions(fid=farmer,question=query)
+        ob.save()
+
+        return redirect('queryPage')
+    
+    return HttpResponse('404-NOT FOUND')
