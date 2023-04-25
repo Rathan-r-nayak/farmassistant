449a53541bc1d63ae441ac1f76a8afe7387ad362
Rathan-r-nayak <rathannayak300@gmail.com>
2023-04-25 14:48:30 +0530

    adding methods to handle functioning of discussapp

--- farmassistant/urls.py
+++ farmassistant/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('welcomeapp.urls')),
-    path('discuss',include('discussapp.urls'))
+    path('discuss/',include('discussapp.urls'))
     
 ]
