6558eee28c8b6dfeaa63617d3bf8adde3d6218be
Rathan-r-nayak <rathannayak300@gmail.com>
2023-04-26 06:47:08 +0530

    updated sql file

--- farmassistant.sql
+++ farmassistant.sql
@@ -17,6 +17,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+
+DROP SCHEMA IF EXISTS farmassistant;
+CREATE SCHEMA farmassistant;
+USE farmassistant;
+
 --
 -- Database: `farmassistant`
 --
